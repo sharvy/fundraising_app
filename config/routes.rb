df0cccd7b7779ae09c143d232b443ced210a96f5
Sharvy Ahmed <sharvy2008@gmail.com>
2023-06-11 02:07:24 +0600

    Formatted properly.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   resources :campaigns
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "campaigns#index"
+  root 'campaigns#index'
 end
