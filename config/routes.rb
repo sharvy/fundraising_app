f23c78dcbb7000f80bfa2405a8921f00d1a351bd
Sharvy Ahmed <sharvy2008@gmail.com>
2023-06-13 22:06:08 +0600

    Add user authentication using devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks'
+  }
+
+  get 'pages/home'
   resources :campaigns
-  root 'campaigns#index'
+  root 'pages#home'
 end
